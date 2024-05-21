8120c0e96fd06a1fe1011ec64c78c65381888aaf
benjamin <benjamin.trauscheid@posteo.de>
2024-05-21 09:31:04 +0200

    changed file names

--- src/ascii_art.py
+++ src/ascii_art.py
@@ -28,6 +28,6 @@ line_four = """    '''
 hangman_stages = []
 
 for x in range(0, 5):
-    f = open('resources/hangman_stage_{fx}'.format(fx=x), 'r')
+    f = open('resources/hangman_stage_{fx}.txt'.format(fx=x), 'r')
     hangman_stages.append(f.read())
     f.close()
